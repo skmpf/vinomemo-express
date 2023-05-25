83cccd9ea4c15317af73c825617d23fc24b9db75
Sebastien Kempf <skempf@deezer.com>
2023-05-25 23:01:31 +0200

    test: rewrite authenticate middleware tests

--- src/__tests__/authMiddleware.test.ts
+++ src/__tests__/authMiddleware.test.ts
@@ -1,11 +1,13 @@
 import { NextFunction, Response } from "express";
-import * as jwt from "jsonwebtoken";
-import * as userController from "../controllers/user";
+import jwt from "jsonwebtoken";
+import { getUserById } from "../controllers/user";
 import { authenticate } from "../middlewares/authMiddleware";
 import { CustomRequest } from "../types/express";
 
 // Mock the dependencies and functions used in the authenticate function
-jest.mock("jsonwebtoken");
+jest.mock("jsonwebtoken", () => ({
+  verify: jest.fn(),
+}));
 jest.mock("../controllers/user");
 
 describe("authenticate", () => {
@@ -23,79 +25,55 @@ describe("authenticate", () => {
       send: jest.fn(),
     } as unknown as Response;
     next = jest.fn();
-    process.env.JWT_SECRET = "secret";
   });
 
   afterEach(() => {
     jest.clearAllMocks();
   });
 
-  test("should call next if authentication is successful", async () => {
-    const token = "valid-token";
-    const decodedToken = {
-      user: { _id: "user123" },
-    };
-
-    req.header = jest.fn().mockReturnValueOnce(`Bearer ${token}`);
-    (jwt.verify as jest.Mock).mockReturnValueOnce(decodedToken);
-    (userController.getUserById as jest.Mock).mockResolvedValueOnce(
-      decodedToken.user
-    );
-
+  test("should throw an error if no token is provided", async () => {
     await authenticate(req, res, next);
 
-    console.log(
-      "🚀 ~ file: authMiddleware.test.ts:50 ~ test ~ req.user:",
-      req.user
-    );
-    expect(req.user).toEqual(decodedToken.user);
-    expect(res.status).not.toHaveBeenCalled();
-    expect(res.send).not.toHaveBeenCalled();
-    expect(next).toHaveBeenCalled();
-  });
-
-  test("should return No token provided if no token is provided", async () => {
-    req.header = jest.fn().mockReturnValueOnce(undefined);
-
-    await authenticate(req, res, next);
-
-    expect(req.user).toBeUndefined();
+    expect(req.header).toHaveBeenCalledWith("Authorization");
     expect(res.status).toHaveBeenCalledWith(401);
     expect(res.send).toHaveBeenCalledWith("Unauthorized - No token provided");
     expect(next).not.toHaveBeenCalled();
   });
 
-  test("should return Invalid token if token verification fails", async () => {
-    const token = "invalid-token";
-
-    req.header = jest.fn().mockReturnValueOnce(`Bearer ${token}`);
-    (jwt.verify as jest.Mock).mockImplementationOnce(() => {
-      throw new Error("Invalid token");
-    });
-
-    await authenticate(req, res, next);
-
-    expect(req.user).toBeUndefined();
-    expect(res.status).toHaveBeenCalledWith(401);
-    expect(res.send).toHaveBeenCalledWith("Unauthorized - Invalid token");
-    expect(next).not.toHaveBeenCalled();
-  });
-
-  test("should return User not allowed if user ID is unauthorized", async () => {
-    const token = "valid-token";
+  test("should throw an error if the id in the token does not match the id in the request params", async () => {
     const decodedToken = {
-      user: { _id: "user456" },
+      user: { _id: "user123" },
     };
-
-    req.header = jest.fn().mockReturnValueOnce(`Bearer ${token}`);
+    req.header = jest.fn().mockReturnValueOnce("Bearer valid_token");
+    req.params.id = "user456";
     (jwt.verify as jest.Mock).mockReturnValueOnce(decodedToken);
-    req.params.id = "user123";
 
     await authenticate(req, res, next);
 
-    expect(req.user).toBeUndefined();
+    expect(req.header).toHaveBeenCalledWith("Authorization");
+    expect(jwt.verify).toHaveBeenCalled();
     expect(res.status).toHaveBeenCalledWith(401);
     expect(res.send).toHaveBeenCalledWith("Unauthorized - User not allowed");
     expect(next).not.toHaveBeenCalled();
+  });
+
+  test("should authenticate and call next if a valid token is provided with matching user ID", async () => {
+    const decodedToken = {
+      user: { _id: "user123" },
+    };
+    req.params.id = "user123";
+    req.header = jest.fn().mockReturnValueOnce("Bearer valid_token");
+    (jwt.verify as jest.Mock).mockReturnValueOnce(decodedToken);
+    (getUserById as jest.Mock).mockResolvedValueOnce(decodedToken.user);
+
+    await authenticate(req, res, next);
+
+    expect(req.header).toHaveBeenCalledWith("Authorization");
+    expect(jwt.verify).toHaveBeenCalled();
+    expect(getUserById).toHaveBeenCalledWith("user123");
+    expect(req.user).toEqual({ _id: "user123" });
+    expect(next).toHaveBeenCalled();
+    expect(res.status).not.toHaveBeenCalled();
+    expect(res.send).not.toHaveBeenCalled();
   });
 });
