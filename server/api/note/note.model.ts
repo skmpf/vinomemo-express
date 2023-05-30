7e5a8486725a6fe75a658eb08c6b61f3951b8b0f
Sebastien Kempf <skempf@deezer.com>
2023-05-30 13:57:09 +0200

    fix: update note interface

--- server/api/note/note.model.ts
+++ server/api/note/note.model.ts
@@ -1,4 +1,39 @@
-import mongoose, { InferSchemaType } from "mongoose";
+import mongoose from "mongoose";
+
+export interface INote {
+  information: {
+    name: string;
+    country?: string;
+    region?: string;
+    grapes?: string[];
+    producer?: string;
+    vintage?: number;
+    alcohol?: number;
+  };
+  appearance?: {
+    intensity?: number;
+    color?: string;
+  };
+  nose?: {
+    intensity?: number;
+    aromas?: string[];
+  };
+  palate?: {
+    sweetness?: number;
+    acidity?: number;
+    tannin?: number;
+    alcohol?: number;
+    body?: number;
+    intensity?: number;
+    flavors?: string[];
+    finish?: number;
+  };
+  conclusion?: {
+    comments?: string;
+    rating?: number;
+  };
+  creator: mongoose.Types.ObjectId;
+}
 
 const noteSchema = new mongoose.Schema(
   {
@@ -33,11 +68,13 @@ const noteSchema = new mongoose.Schema(
       comments: String,
       rating: Number,
     },
-    creator: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    creator: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
   },
   { timestamps: true }
 );
 
-export type INote = InferSchemaType<typeof noteSchema>;
-
-export default mongoose.model("Note", noteSchema);
+export default mongoose.model<INote>("Note", noteSchema);
