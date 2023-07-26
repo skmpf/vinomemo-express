24180860a627caef5633eeafd4a4234011b834f4
Sebastien Kempf <skempf@deezer.com>
2023-07-26 08:38:42 +0200

    wip: troubleshoot ci

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,6 @@ module.exports = {
   testMatch: ["**/__tests__/**/*.test.ts"],
   preset: "ts-jest",
   globalSetup: "./__tests__/db/globalSetup.ts",
-  globalTeardown: "./__tests__/db/globalTeardown.ts",
+  // globalTeardown: "./__tests__/db/globalTeardown.ts",
   setupFilesAfterEnv: ["./__tests__/db/setupFile.ts"],
 };
