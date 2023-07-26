30f71b822d1583da670193e457aa8abc0d607936
Sebastien Kempf <skempf@deezer.com>
2023-07-26 08:16:55 +0200

    wip: troubleshoot ci

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,8 @@
-const path = require("path");
-
 module.exports = {
   testEnvironment: "node",
   testMatch: ["**/__tests__/**/*.test.ts"],
   preset: "ts-jest",
-  globalSetup: path.resolve(__dirname, "./__tests__/db/globalSetup.ts"),
-  globalTeardown: path.resolve(__dirname, "./__tests__/db/globalTeardown.ts"),
-  setupFilesAfterEnv: [path.resolve(__dirname, "./__tests__/db/setupFile.ts")],
+  globalSetup: "./__tests__/db/globalSetup.ts",
+  globalTeardown: "./__tests__/db/globalTeardown.ts",
+  setupFilesAfterEnv: ["./__tests__/db/setupFile.ts"],
 };
